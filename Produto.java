c9d525909414e87807c12cfeec3361ccea322e23
igor.souza <igorrs.rib@gmail.com>
2022-09-28 13:05:14 -0300

    creating print

--- Produto.java
+++ Produto.java
@@ -2,4 +2,14 @@ public class Produto {
     String name;
     String fabricante;
     Double preco;
+
+    public Produto(String name, String fabricante, Double preco){
+        this.name=name;
+        this.fabricante=fabricante;
+        this.preco=preco;
+    }
+
+    public void getProduto(){
+        System.out.println(name);
+    }
 }
--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main{
     public static void main(String args[]){
-        Produto p = new Produto();
+        Produto p = new Produto("roupa","eu",50.55);
         Lote l = new Lote();
+        p.getProduto();
     }
 }
